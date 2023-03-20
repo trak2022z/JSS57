7ead2f8dfc06f95d65e7853981d1bae46fbb8e66
Tom Krok <91782249+trak2022z@users.noreply.github.com>
2023-03-20 16:39:02 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,16 @@
+const body = document.querySelector("body");
+const clickTarget = document.getElementById("click-target");
+const mouseOverTarget = document.getElementById("mouse-over-target");
+
+let toggle = false;
+function makeBackgroundYellow() {
+  body.style.backgroundColor = toggle ? "white" : "yellow";
+
+  toggle = !toggle;
+}
+
+clickTarget.addEventListener("click", makeBackgroundYellow, false);
+
+mouseOverTarget.addEventListener("mouseover", () => {
+  clickTarget.removeEventListener("click", makeBackgroundYellow, false);
+});
